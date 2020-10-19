42a696646bfe320d8a4f603ce529859bccbfd455
Artyom Sommer <litovskys@gmail.com>
2020-10-19 20:23:31 +0300

    Updates

---
+++ precache-manifest.4e1a23ae5700fec49e7ce389403b077c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e78e6f3ea549325261c18719b8ea6a7",
+    "url": "/tiles-game/index.html"
+  },
+  {
+    "revision": "9191116c2e5aecf57ec1",
+    "url": "/tiles-game/static/css/main.16488112.chunk.css"
+  },
+  {
+    "revision": "52bb37d6d4430460aff7",
+    "url": "/tiles-game/static/js/2.e611cc82.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/tiles-game/static/js/2.e611cc82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9191116c2e5aecf57ec1",
+    "url": "/tiles-game/static/js/main.54716b42.chunk.js"
+  },
+  {
+    "revision": "3fb2fd732626de623c11",
+    "url": "/tiles-game/static/js/runtime-main.7ec06ffc.js"
+  }
+]);
